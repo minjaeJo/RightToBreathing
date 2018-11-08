c76747ce64db1798c9bc2a0d216cc61e5b535c65
minjae <alswo2722@naver.com>
2018-11-08 19:24:02 +0900

    404 Not Found 페이지 제작

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 
 import StartGate from '../view/StartGate.vue'
 import Main from '../view/Main.vue'
+import NotFound404 from '../view/NotFound404'
 
 Vue.use(Router)
 
@@ -10,13 +11,20 @@ export default new Router({
     mode:'history',
     routes: [
         {
-            name: StartGate,
+            name: 'StartGate',
             path: '/',
             component: StartGate
-        }, {
-            name: Main,
+        },
+        {
+            name: 'Main',
             path: '/main',
             component: Main
+        },
+        {
+            name: 'NotFound404',
+            path: '*',
+            component: NotFound404
         }
+
     ]
 })
