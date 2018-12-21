133a5e754f27ed2e2d39b13e44511e8b13592628
minjae <alswo2722@naver.com>
2018-12-21 23:40:04 +0900

    table 만들기

--- src/view/Main.vue
+++ src/view/Main.vue
@@ -1,10 +1,28 @@
 <template>
     <div id="main" class="window-container">
-        <div class="input-contaier">
-            <input v-model="camera_position_x" placeholder="x좌표">
-            <input v-model="camera_position_y" placeholder="y좌표">
-            <input v-model="camera_position_z" placeholder="z좌표">
-            <b-btn variant="green" @click="changeCamera">카메라 변경</b-btn>
+        <div class="navbar-container">
+            <div class="title">쉼쉴권리</div>
+            <div class="description">숨쉴권리 컨소시엄이란?</div>
+        </div>
+        <div class="table-container">
+            <div class="table-header">
+                <div class="table-td">상호명 / 위치</div>
+                <div class="table-td">|</div>
+                <div class="table-td">공기질 지표</div>
+            </div>
+            <div class="body-scroll">
+                <div class="table-body">
+                    <div class="table-row" v-for="i in 20" :key="i">
+                        <div class="table-td">서울전자 / 청계-다203 (2층)</div>
+                        <div class="table-td"></div>
+                        <div class="table-td">
+                            <b-progress :value="50" :max="100"></b-progress>
+                            <div class="point">2<span class="max-point">/ 10</span></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </template>
@@ -13,12 +31,11 @@
 import * as THREE from 'three'
 import GLTFLoader from 'three-gltf-loader'
 import OrbitControls from 'three-orbitcontrols'
-import Dat from "dat.gui";
 
 export default {
     mounted() {
-        this.init()
-        this.onAnimationFrameHandler()
+        // this.init()
+        // this.onAnimationFrameHandler()
     },
     data() {
         return {
@@ -27,11 +44,7 @@ export default {
             camera: '',
             loader: '',
             model: '',
-            control: '',
-            gui: '',
-            camera_position_x: '',
-            camera_position_y: '',
-            camera_position_z: ''
+            control: ''
         }
     },
     methods: {
@@ -44,7 +57,6 @@ export default {
             this.initCamera();
             this.initRenderer();
             this.initLight();
-            // this.initDatGUI();
             const windowResizeHanlder = () => {
                 const { innerHeight, innerWidth } = window;
                 this.renderer.setSize(innerWidth, innerHeight);
@@ -56,9 +68,6 @@ export default {
 
             document.getElementById("main").appendChild(this.renderer.domElement);
         },
-        // initDatGUI() {
-        //     this.gui = new Dat.GUI();
-        // },
         initCamera() {
             this.camera.position.set(6,3,500);
             this.camera.lookAt(new THREE.Vector3(100,100,0));
@@ -86,17 +95,13 @@ export default {
             this.controls = new OrbitControls( this.camera, this.renderer.domElement );
             // this.controls.target.set( 0, - 0.2, - 0.2 );
             this.controls.target.set( 0, 10, 0 );
-            this.controls.autoRotate = true
+            // this.controls.autoRotate = true
             this.controls.update();
         },
         onAnimationFrameHandler() {
             window.requestAnimationFrame(this.onAnimationFrameHandler);
             this.controls.update();
             this.renderer.render(this.scene, this.camera);
-        },
-        changeCamera() {
-            console.log('hi')
-            this.camera.position.set(this.camera_position_x,this.camera_position_y,this.camera_position_z)
         }
     }
 }
@@ -105,14 +110,65 @@ export default {
 <style scoped>
 .window-container {
     position: absolute;
+    width: 100%;
+    height: 100%;
 }
-.input-container {
+.navbar-container {
     position: absolute;
     z-index: 1000px;
+    top: 0px;
+    width: 100%;
+    height: 60px;
+    background: #4b4b4b;
 }
-.input-container .block {
-    margin-bottom: 20px;
-    height: 30px;
+.navbar-container .title {
+    display: inline-block;
+    float: left;
+    margin-left: 30px;
+    height: 60px;
+    line-height: 60px;
+    font-size: 30px;
+    font-family: 'InkLipquid';
+    color: #09fcdc;
+}
+.navbar-container .description {
+    display: inline-block;
+    float: right;
+    height: 60px;
+    line-height: 60px;
+    margin-right: 30px;
+    color: #09fcdc;
+}
+.table-container {
+    position: absolute;
+    z-index: 1000px;
+    top: 40px;
+    width: 500px;
+    height: 90%;
+    right: 0;
+    margin: 30px;
+    background: #383838;
+    border: 1px solid #09fcdc
+}
+.table-td:nth-child(1) {
+    padding-left: 28px;
+    width: 290px;
+}
+.table-td:nth-child(2) {
+    width: 10px;
+}
+.table-td:nth-child(3) {
+    padding-left: 28px;
+    width: 200px;
+}
+.point {
+    display: inline-block;
+    margin-left: 20px;
+}
+.max-point {
+    opacity: 0.4;
+    font-size: 11px;
+    margin-left: 5px;
 }
 </style>
 
